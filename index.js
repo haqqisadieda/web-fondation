7ca9d8d0ae0cbe21066ca59cf429fa2e1a11925a
pandegahaes <haqqisadiedapandega@gmail.com>
2022-02-12 09:32:37 +0700

    Mengubah nama app.js ke index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (req, res) => {
